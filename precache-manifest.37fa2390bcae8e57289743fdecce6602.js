fba40fdd5e6ace81ab9e8e8f4340a27a5fafdf75
hanxia <hanxia@cn.ibm.com>
2019-09-20 08:28:21 +0800

    Updates

---
+++ precache-manifest.37fa2390bcae8e57289743fdecce6602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996e8a310706eec1a375769fdc8563a1",
+    "url": "/SwiftTest/index.html"
+  },
+  {
+    "revision": "cfc0f1c08f3964aacde9",
+    "url": "/SwiftTest/static/css/main.d9fe0dda.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/SwiftTest/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "cfc0f1c08f3964aacde9",
+    "url": "/SwiftTest/static/js/main.1ee0b80b.chunk.js"
+  },
+  {
+    "revision": "bb654933e50259ce2c3c",
+    "url": "/SwiftTest/static/js/runtime~main.235eaba1.js"
+  }
+]);
